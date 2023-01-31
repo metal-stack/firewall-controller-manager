fb7c4b0cae4c68cf6fb2f2c15cc38ca68b0de264
Gerrit <info@gerritschwerthelm.de>
2023-01-31 12:46:00 +0100

    Rename to firewall.metal-stack.io group.
    Otherwise it will conflict with the current v1 firewall, requiring a conversion which is not possible.

--- api/v2/groupversion_info.go
+++ api/v2/groupversion_info.go
@@ -1,13 +1,13 @@
 // +kubebuilder:object:generate=true
-// +groupName=metal-stack.io
+// +groupName=firewall.metal-stack.io
 //
-// +kubebuilder:webhook:path=/validate-metal-stack-io-v2-firewall,mutating=false,failurePolicy=fail,groups=metal-stack.io,resources=firewalls,verbs=create;update,versions=v2,name=firewall.metal-stack.io,sideEffects=None,admissionReviewVersions=v1
-// +kubebuilder:webhook:path=/validate-metal-stack-io-v2-firewallset,mutating=false,failurePolicy=fail,groups=metal-stack.io,resources=firewallsets,verbs=create;update,versions=v2,name=firewallset.metal-stack.io,sideEffects=None,admissionReviewVersions=v1
-// +kubebuilder:webhook:path=/validate-metal-stack-io-v2-firewalldeployment,mutating=false,failurePolicy=fail,groups=metal-stack.io,resources=firewalldeployments,verbs=create;update,versions=v2,name=firewalldeployment.metal-stack.io,sideEffects=None,admissionReviewVersions=v1
+// +kubebuilder:webhook:path=/validate-firewall-metal-stack-io-v2-firewall,mutating=false,failurePolicy=fail,groups=firewall.metal-stack.io,resources=firewalls,verbs=create;update,versions=v2,name=firewall.metal-stack.io,sideEffects=None,admissionReviewVersions=v1
+// +kubebuilder:webhook:path=/validate-firewall-metal-stack-io-v2-firewallset,mutating=false,failurePolicy=fail,groups=firewall.metal-stack.io,resources=firewallsets,verbs=create;update,versions=v2,name=firewallset.metal-stack.io,sideEffects=None,admissionReviewVersions=v1
+// +kubebuilder:webhook:path=/validate-firewall-metal-stack-io-v2-firewalldeployment,mutating=false,failurePolicy=fail,groups=firewall.metal-stack.io,resources=firewalldeployments,verbs=create;update,versions=v2,name=firewalldeployment.metal-stack.io,sideEffects=None,admissionReviewVersions=v1
 //
-// +kubebuilder:webhook:path=/mutate-metal-stack-io-v2-firewall,mutating=true,failurePolicy=fail,groups=metal-stack.io,resources=firewalls,verbs=create,versions=v2,name=firewall.metal-stack.io,sideEffects=None,admissionReviewVersions=v1
-// +kubebuilder:webhook:path=/mutate-metal-stack-io-v2-firewallset,mutating=true,failurePolicy=fail,groups=metal-stack.io,resources=firewallsets,verbs=create,versions=v2,name=firewallset.metal-stack.io,sideEffects=None,admissionReviewVersions=v1
-// +kubebuilder:webhook:path=/mutate-metal-stack-io-v2-firewalldeployment,mutating=true,failurePolicy=fail,groups=metal-stack.io,resources=firewalldeployments,verbs=create,versions=v2,name=firewalldeployment.metal-stack.io,sideEffects=None,admissionReviewVersions=v1
+// +kubebuilder:webhook:path=/mutate-firewall-metal-stack-io-v2-firewall,mutating=true,failurePolicy=fail,groups=firewall.metal-stack.io,resources=firewalls,verbs=create,versions=v2,name=firewall.metal-stack.io,sideEffects=None,admissionReviewVersions=v1
+// +kubebuilder:webhook:path=/mutate-firewall-metal-stack-io-v2-firewallset,mutating=true,failurePolicy=fail,groups=firewall.metal-stack.io,resources=firewallsets,verbs=create,versions=v2,name=firewallset.metal-stack.io,sideEffects=None,admissionReviewVersions=v1
+// +kubebuilder:webhook:path=/mutate-firewall-metal-stack-io-v2-firewalldeployment,mutating=true,failurePolicy=fail,groups=firewall.metal-stack.io,resources=firewalldeployments,verbs=create,versions=v2,name=firewalldeployment.metal-stack.io,sideEffects=None,admissionReviewVersions=v1
 package v2
 
 import (
@@ -17,7 +17,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "metal-stack.io", Version: "v2"}
+	GroupVersion = schema.GroupVersion{Group: "firewall.metal-stack.io", Version: "v2"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
