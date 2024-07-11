0fdf88a5cf3b67f121b19f41d8c4f24e4e2363da
Gerrit <info@gerritschwerthelm.de>
2024-07-11 11:41:29 +0200

    Fix.

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"log"
 	"log/slog"
-	"net/http"
 	"os"
 	"time"
 
@@ -39,19 +38,6 @@ import (
 const (
 	metalAuthHMACEnvVar = "METAL_AUTH_HMAC"
 )
-
-func healthCheckFunc(log *slog.Logger, seedClient client.Client, namespace string) func(req *http.Request) error {
-	return func(req *http.Request) error {
-		log.Debug("health check called")
-
-		fws := &v2.FirewallList{}
-		err := seedClient.List(req.Context(), fws, client.InNamespace(namespace))
-		if err != nil {
-			return fmt.Errorf("unable to list firewalls in namespace %s", namespace)
-		}
-		return nil
-	}
-}
 
 func main() {
 	var (
